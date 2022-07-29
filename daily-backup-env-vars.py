0400b69a73c2600bcddbf4886f52bad1de9eb025
Vic P <vic4key@gmail.com>
2022-07-30 03:48:59 +0700

    Initial

--- daily-backup-env-vars.py
+++ daily-backup-env-vars.py
@@ -8,9 +8,17 @@ __author__  = "Vic P."
 __summary__ = "The summary of the extension"
 __url__     = "https://github.com/vic4key/VAppExt.git"
 
-def reg_backup(suffix_file_name, reg_key):
+def open_folder_in_explorer(dir):
+  subprocess.run(["explorer", os.path.realpath(dir)])
+  return
+
+def get_backup_folder():
   dir = os.path.join(VExt.API.extensions_dir(), "backup-environment-variables")
   if not os.path.exists(dir): os.mkdir(dir) # make sure the backup folder exists
+  return dir
+
+def backup_registry_key(suffix_file_name, reg_key):
+  dir = get_backup_folder()
   time = datetime.now().strftime("%Y%d%m_%H%M%S") # eg. 20220730_1234
   subprocess.call(
     f"regedit /e \"{dir}\\{time}_{suffix_file_name}.reg\" \"{reg_key}\"", creationflags=subprocess.CREATE_NO_WINDOW)
@@ -18,8 +26,8 @@ def reg_backup(suffix_file_name, reg_key):
 
 def backup_environment_variables():
   try:
-    reg_backup("usr", R"HKEY_CURRENT_USER\Environment")
-    reg_backup("sys", R"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment")
+    backup_registry_key("usr", R"HKEY_CURRENT_USER\Environment")
+    backup_registry_key("sys", R"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment")
   except Exception as e:
     print(e)
   return
@@ -44,8 +52,13 @@ class Window(VExt.UI.Window):
         "children": [],
       },
       {
-        "caption": "Information",
+        "caption": "Open Backup Folder",
         "index": 2,
+        "children": [],
+      },
+      {
+        "caption": "Information",
+        "index": 3,
         "children": [],
       },
     ]
@@ -55,7 +68,11 @@ class Window(VExt.UI.Window):
     if idx == 1:
       backup_environment_variables()
       print("Environment Variables Backup Succeed")
-    elif idx == 2: VExt.API.message_box(f"{__name__} {__version__}\n\nCopyright (c) {__author__}", f"Information")
+    if idx == 2:
+      dir = get_backup_folder()
+      open_folder_in_explorer(dir)
+    elif idx == 3:
+      VExt.API.message_box(f"{__name__} {__version__}\n\nCopyright (c) {__author__}", f"Information")
     return
 
   def on_menu_update(self, idx, lp):
